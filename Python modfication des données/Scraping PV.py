9d059763c4957e3b7b6a4bfa414adbc5a8dfc9fc
Sofiane <cheriette.sofiane@gmail.com>
2025-06-05 18:38:12 +0200

    MAJ Classif avec Episodes Prime

--- Python modfication des données/Scraping PV.py
+++ Python modfication des données/Scraping PV.py
@@ -3,7 +3,7 @@ import re
 import html
 import pandas as pd
 
-
+'''
 probdef = []
 proburl = []
 
@@ -69,9 +69,10 @@ def classifPV(url):
             return age.group(1)
         except Exception as e:
             proburl.append((url, e))
+print(classifPV('https://app.primevideo.com/detail?gti=amzn1.dv.gti.24b8776e-16e0-e3e3-67e8-b144e97d7728'))
 
 
-'''  #Series ou films
+  #Series ou films
 dataplat=pd.read_csv('dataplatfilms.csv', index_col=0)
 eta = 1
 
@@ -92,7 +93,7 @@ print(len(probdef))
 print(proburl)
 print(len(proburl))
 dataplat.to_csv('dataplatfilms.csv')
-'''
+
 #-------------------------- Épisodes ----------------------------
 
 #print(classifPV('https://www.primevideo.com/-/fr/detail/0IQX4B70WS4VB5JSB4GBTCACON'))
@@ -121,7 +122,55 @@ print(len(probdef))
 print(proburl)
 print(len(proburl))
 dataepisode.to_csv('dataepisodes.csv')
+'''
+
+probdef = []
+proburl = []
 
 
+
+def ttsaisons(url):
+    headers={
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36'
+    }
+
+    site=requests.get(url, headers=headers)
+    if site.status_code==200:
+        if 'fr' in url:
+            saisons=re.findall(r'"seasonLink":"(/-/fr/detail/\w+/ref=atv_dp_season_select_s\d+)"', site.text)
+        else:
+            saisons = re.findall(r'"seasonLink":"(/detail/\w+/ref=atv_dp_season_select_s\d+)"', site.text)
+            for i in range(len(saisons)):
+                saisons[i]="/-/fr"+saisons[i]
+        for i in range(len(saisons)):
+            saisons[i]='https://www.primevideo.com'+saisons[i]
+        return saisons
+
+
+def classifPV(url):
+    serie=True
+    if 'app' in url:
+        url=url+'&language=fr-FR'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36'
+    }
+    if serie==True:
+        saisons=ttsaisons(url)
+        classifep = []
+        classifsais=[]
+        numep=0
+        try:
+            #print(saisons)
+            for saison in saisons:
+                site=requests.get(saison, headers=headers)
+                #print(site.text)
+                if site.status_code==200:
+                    detailepi=re.findall(r'<span class="_36qUej izvPPq"><span>S\. (\d+) ÉP\. (\d+)</span>.+?aria-label="Ce contenu est classé ([^"]+)"', site.text)
+                    print (detailepi)
+
+        except Exception as e:
+            proburl.append((url, e))
+
+print(classifPV('https://app.primevideo.com/detail?gti=amzn1.dv.gti.8b96ae49-3860-49a5-8d15-7bd50c2ef008'))
 
 
